cbd7dcf79633a4710cb1b6f847765e159f25ee83
N <thecatty@yandex.ru>
2024-01-21 14:02:12 +0300

    all

---
+++ src/day00_ex05.sql
@@ -0,0 +1,6 @@
+SELECT
+    (SELECT name
+     FROM person
+     WHERE person.id = person_order.person_id) AS NAME
+FROM person_order
+WHERE person_order.menu_id IN (13, 14, 18) AND DATE(person_order.order_date) = '2022-01-07';
---
+++ src/day00_ex06.sql
@@ -0,0 +1,16 @@
+SELECT
+    (SELECT name
+     FROM person
+     WHERE person.id = person_order.person_id) AS NAME,
+    CASE
+        WHEN
+            (SELECT name
+             FROM person
+             WHERE person.id = person_order.person_id) = 'Denis'
+        THEN
+            TRUE
+        ELSE
+            FALSE
+    END AS check_name
+FROM person_order
+WHERE person_order.menu_id IN (13, 14, 18) AND DATE(person_order.order_date) = '2022-01-07';
